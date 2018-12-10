9563a0969c61018944ad48b3828b271cff96a9cd
Christopher Pace <myiahsade@gmail.com>
2018-12-10 09:32:54 -0600

    check in

---
+++ an.py
@@ -0,0 +1,10 @@
+array = [[4, 5, 6], [2, 3, 4], [7, 5, 3]]
+
+
+ar1=array[0][0]+array[1][0]+array[2][0]
+ar2= array[0][1]+array[1][1]+array[2][1]
+ar3 =array[0][2]+array[1][2]+array[2][2]
+
+print(ar1)
+print(ar2)
+print(ar3)
